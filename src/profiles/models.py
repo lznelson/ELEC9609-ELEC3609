863c463c0198e865f033ec490e5e0132214a1d68
FrankieWei727 <31089132+FrankieWei727@users.noreply.github.com>
2017-10-18 12:08:50 +1100

    delete file

--- src/profiles/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class profile(models.Model):
-	name = models.CharField(max_length=120)
-	description = models.TextField(default='description default text')
-
-	def _unicode_(self):
-		return self.name
