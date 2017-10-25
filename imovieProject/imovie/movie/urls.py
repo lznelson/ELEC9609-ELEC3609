0e024c152c477f2bafdde16f5b071a943ab88448
jche8218 <31757202+jche8218@users.noreply.github.com>
2017-10-25 15:11:50 +1100

    url
    add more detail on url

---
+++ imovieProject/imovie/movie/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+	
+	url(r'^like/$', views.movie_like, name='movie_like'),	
+	url(r'^movie-comment/(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.movie_comment, 
+		name='movie_comment'),
+	url(r'^movie-ranking/$', views.movie_ranking, name='movie_ranking'),
+	url(r'^(?P<category_slug>[-\w]+)/$', views.movie_list, 
+		name='movie_list_by_category'),
+
+]
