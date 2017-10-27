e6980d8dfa246e0c388ed11c0d077c31cd91e680
lznelson <zliu0098@uni.sydney.edu.au>
2017-10-27 16:38:23 +1100

    update movies

--- imovieProject/imovie/movie/models.py
+++ imovieProject/imovie/movie/models.py
@@ -68,4 +68,23 @@ class Movie(models.Model):
 		return reverse('movies:movie_comment',
 						args=[self.id,self.slug])
 
+ class Activity(models.Model):
+ 
+ 	title = models.CharField(max_length=200)
+ 	slug = models.SlugField(max_length=200)
+ 	body = models.TextField()
+ 	active = models.BooleanField(default=True)
+ 	created = models.DateTimeField(auto_now_add=True)
+ 	date = models.DateField(blank=True, null=True)
+ 	todate = models.DateField(blank=True,null=True)
+ 
+ 	class Meta:
+ 		ordering = ('created',)
+ 
+ 	def __str__(self):
+ 		return self.title
+ 
+ 	def get_absolute_url(self):
+ 		return reverse('movies:activity_detail',
+ 						args=[self.id,self.slug])
 
--- imovieProject/imovie/movie/urls.py
+++ imovieProject/imovie/movie/urls.py
@@ -12,3 +12,14 @@ urlpatterns = [
 
 ]
 
+urlpatterns = [
+ 
+ 	url(r'^movie-detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.movie_detail, 
+ 		name='movie_detail'),
+ 	url(r'^movie-list/$', views.movie_list, name='movie_list'),
+ 
+ 	url(r'^activity-list/$', views.activity_list, name='activity_list'),
+ 	url(r'^activity-detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.activity_detail, 
+ 		name='activity_detail'),
+ 
+ ]
