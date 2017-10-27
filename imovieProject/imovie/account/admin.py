9c1d8fb90776968a66ab1cd6f7628f69ae213356
00ox-xo00 <31852469+00ox-xo00@users.noreply.github.com>
2017-10-27 18:16:09 +1100

    3rd try

--- imovieProject/imovie/account/admin.py
+++ imovieProject/imovie/account/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'date_of_birth','id']
+    list_filter = ['user']
+
+admin.site.register(Profile, ProfileAdmin)
--- imovieProject/imovie/account/views.py
+++ imovieProject/imovie/account/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpResponseRedirect
+﻿from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
@@ -107,4 +107,72 @@ def dashboard(request):
 				'shortcomments':shortcomments,
 				'comments':comments,
 				'movies':movies})
+
+
+@login_required
+def edit(request):
+	if request.method == 'POST':
+		user_form = UserEditForm(instance=request.user,
+								 data=request.POST)
+		profile_form = ProfileEditForm(instance=request.user.profile,
+									   data=request.POST,
+									   files=request.FILES)
+		if user_form.is_valid() and profile_form.is_valid():
+			user_form.save()
+			profile_form.save()
+			messages.success(request, 'Profile updated successfully')
+		else:
+			messages.error(request, 'Error updating your profile')
+	else:
+		user_form = UserEditForm(instance=request.user)
+		profile_form = ProfileEditForm(instance=request.user.profile)
+	return render(request, 'account/edit.html', {'user_form': user_form,
+												 'profile_form': profile_form})
+
+@login_required
+def user_detail(request,username):
 	
+	user= get_object_or_404(User,username=username)
+	shortcomments = ShortComment.objects.filter(author_id=user)
+	comments = Comment.objects.filter(author_id=user)
+	movies = Movie.objects.filter(users_like=user)
+	return render(request,
+				'account/user_detail.html',
+				{'user':user,
+				'shortcomments':shortcomments,
+				'comments':comments,
+				'movies':movies})
+
+def movie_detail(request, id, slug):
+	movie = get_object_or_404(Movie,
+							id=id,
+							slug=slug)
+	r.zincrby('movie_ranking',movie.id,1)
+	return 
+
+@login_required
+def home(request):
+	movies = Movie.objects.all()
+	shortcomments = ShortComment.objects.all()
+	comments = Comment.objects.all()
+
+
+	form = SearchForm()
+	if 'query' in request.GET:
+		form = SearchForm(request.GET)
+		if form.is_valid():
+			cd = form.cleaned_data
+			results = SearchQuerySet().models(Movie).filter(content=cd['query']).load_all()
+			# count total results
+			total_results = results.count()
+		return render(request, 'movies/movie/list.html',
+								{'form': form,
+								'cd': cd,
+								'results': results,
+								'total_results': total_results})
+	return render(request,
+				'account/home.html',
+				{'section': 'home',
+				'movies':movies,
+				'shortcomments':shortcomments,
+				'comments':comments})	
